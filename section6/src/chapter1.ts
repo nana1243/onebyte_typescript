1eb4ff257195a5195eeda3219f662f6e347c6fd5
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-09-23 07:47:50 +0900

    fea)[9/23] typescript의 클래스

---
+++ section6/src/chapter1.ts
@@ -0,0 +1,43 @@
+/* `typescript`의 class*/
+
+class Employee {
+    name: string;
+    age : number;
+    position : string;
+
+    constructor({name, age, position} : {name: string, age:number, position:string}) {
+        this.name = name;
+        this.age = age;
+        this.position = position
+    }
+    work() :void{
+        console.log('일함')
+    }
+
+}
+
+class ExecutiveOfficer extends Employee{
+    officeNumber:number;
+
+    constructor({name,age, position, officeNumber}) {
+        super({name,age,position})
+        this.officeNumber = officeNumber
+    }
+}
+
+
+const employeeA = new Employee({
+    name : 'hennie',
+    age : 24,
+    position : 'developer'
+})
+
+// 클래스가 타입으로도 됨
+const employeeC : Employee = {
+    name : 'hennie',
+    age : 20,
+    position : 'test',
+    work() {
+    }
+}
+
