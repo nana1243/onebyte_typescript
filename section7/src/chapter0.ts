e0092db3bb94935f07f078402e7842d61e07876d
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-09-29 07:36:26 +0900

    fea)[9/29] 제네릭 함수

--- section7/src/chapter0.ts
+++ section7/src/chapter0.ts
@@ -1,5 +1,8 @@
-/*Generic*/
+/*Generic
+*  제네릭 함수란? 타입변수와 함께 여러 타입의 값을 인수로 받아서 범용적으로 사용할 수 있는 함수를 의
+* */
 
+// T : 타입 변수
 function func<T>(value:T) :T{
     return value;
 }
@@ -7,3 +10,4 @@ function func<T>(value:T) :T{
 const result1 = func<string>('hello');
 const result2 = func<number>(123);
 const result3 = func<boolean>(true);
+const result4 = func<[number, number, number]>([1,2,3]); // Tuple 타입으로 추론
